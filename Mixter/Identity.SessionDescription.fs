64d4647d02df3f1b4fa6ad187875e59fc38ee52f
fpellet <florent.pellet@gmail.com>
2016-05-16 10:24:03 +0200

    Split Identity file

---
+++ Mixter/Identity.SessionDescription.fs
@@ -0,0 +1,12 @@
+﻿module Mixter.Domain.Identity.SessionDescription
+
+open System
+open UserIdentity
+open Session
+
+type SessionDescription = { SessionId: SessionId; UserId: UserId }
+
+let apply event = 
+    match event with
+    | UserConnected e -> Some { SessionId = e.SessionId; UserId = e.UserId }
+    | UserDisconnected e -> None
---
+++ Mixter/Identity.UserIdentity.fs
@@ -0,0 +1,25 @@
+﻿module Mixter.Domain.Identity.UserIdentity
+
+open System
+
+type UserId = UserId of string
+        
+type Event = 
+    | UserRegistered of UserRegisteredEvent
+and UserRegisteredEvent = { UserId: UserId }
+
+type DecisionProjection = 
+    | UnregisteredUser
+    | RegisteredUser of RegisteredUser
+and RegisteredUser = { UserId: UserId }
+
+let register userId =
+    [ UserRegistered { UserId = userId } ]
+
+let private applyOne decisionProjection event =
+    match (decisionProjection, event) with
+    | (UnregisteredUser, UserRegistered e) -> RegisteredUser { UserId = e.UserId }
+    | _ -> failwith "Invalid transition"
+
+let apply events =
+    Seq.fold applyOne UnregisteredUser events
