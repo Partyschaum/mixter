4431c03b762cb4b445324c008fa111cede5e2159
fpellet <florent.pellet@gmail.com>
2016-05-21 20:07:26 +0200

    When follow Then UserFollowed is returned

---
+++ Mixter.Tests/Core.Subscription.fs
@@ -0,0 +1,17 @@
+﻿namespace Mixter.Tests.Domain.Core.Subscription
+
+open NUnit.Framework
+open FsUnit
+open System
+open Mixter.Domain.Identity.UserIdentity
+open Mixter.Domain.Core.Subscription
+
+[<TestFixture>]
+type ``Subscription should`` ()=
+    [<Test>] 
+    member x.``When follow Then UserFollowed is returned`` () =
+        let follower = UserId "follower@mix-it.fr"
+        let followee = UserId "followee@mix-it.fr"
+
+        follow follower followee
+            |> should equal [UserFollowed { SubscriptionId = { Follower = follower; Followee = followee } }]
---
+++ Mixter/Core.Subscription.fs
@@ -0,0 +1,14 @@
+﻿module Mixter.Domain.Core.Subscription
+
+open Mixter.Domain.Identity.UserIdentity
+
+type SubscriptionId = { Follower: UserId; Followee: UserId }
+
+type Event = 
+    | UserFollowed of UserFollowed
+and UserFollowed = { SubscriptionId: SubscriptionId }
+
+let follow follower followee =
+    [ UserFollowed { SubscriptionId = { Follower = follower; Followee = followee } } ]
+
+
