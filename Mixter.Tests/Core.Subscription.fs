9079d7df02eda4bb02a2f88326ef23d31e6b2a65
fpellet <florent.pellet@gmail.com>
2016-05-21 20:28:50 +0200

    When unfollow Then UserUnfollowed is returned When notify follower Then FolloweeMessageQuacked

--- Mixter.Tests/Core.Subscription.fs
+++ Mixter.Tests/Core.Subscription.fs
@@ -4,6 +4,7 @@ open NUnit.Framework
 open FsUnit
 open System
 open Mixter.Domain.Identity.UserIdentity
+open Mixter.Domain.Core.Message
 open Mixter.Domain.Core.Subscription
 
 [<TestFixture>]
@@ -15,3 +16,22 @@ type ``Subscription should`` ()=
 
         follow follower followee
             |> should equal [UserFollowed { SubscriptionId = { Follower = follower; Followee = followee } }]
+
+    [<Test>] 
+    member x.``When unfollow Then UserUnfollowed is returned`` () =
+        let subscription = { Follower = UserId "follower@mix-it.fr"; Followee = UserId "followee@mix-it.fr" }
+
+        [UserFollowed { SubscriptionId = subscription }]
+            |> apply
+            |> unfollow
+            |> should equal [UserUnfollowed { SubscriptionId = subscription }]
+
+    [<Test>] 
+    member x.``When notify follower Then FolloweeMessageQuacked`` () =
+        let subscription = { Follower = UserId "follower@mix-it.fr"; Followee = UserId "followee@mix-it.fr" }
+        let message = MessageId.generate()
+
+        [UserFollowed { SubscriptionId = subscription}]
+            |> apply
+            |> notifyFollower message
+            |> should equal [FolloweeMessageQuacked { SubscriptionId = subscription; Message = message}]
--- Mixter/Core.Subscription.fs
+++ Mixter/Core.Subscription.fs
@@ -1,14 +1,40 @@
 ﻿module Mixter.Domain.Core.Subscription
 
 open Mixter.Domain.Identity.UserIdentity
+open Mixter.Domain.Core.Message
 
 type SubscriptionId = { Follower: UserId; Followee: UserId }
 
 type Event = 
     | UserFollowed of UserFollowed
+    | UserUnfollowed of UserUnfollowed
+    | FolloweeMessageQuacked of FolloweeMessageQuacked
 and UserFollowed = { SubscriptionId: SubscriptionId }
+and UserUnfollowed = { SubscriptionId: SubscriptionId }
+and FolloweeMessageQuacked = { SubscriptionId: SubscriptionId; Message: MessageId }
+
+type DecisionProjection =
+    | NoSubscription
+    | Active of SubscriptionId
+    | Desactive of SubscriptionId
 
 let follow follower followee =
     [ UserFollowed { SubscriptionId = { Follower = follower; Followee = followee } } ]
 
+let unfollow decisionProjection =
+    match decisionProjection with
+    | Active subscriptionId -> [ UserUnfollowed { SubscriptionId = subscriptionId } ]
+    | _ -> []
 
+let notifyFollower message decisionProjection =
+    match decisionProjection with
+    | Active subscriptionId -> [ FolloweeMessageQuacked { SubscriptionId = subscriptionId; Message = message } ]
+    | _ -> []
+
+let applyOne decisionProjection event =
+    match event with
+    | UserFollowed e -> Active e.SubscriptionId
+    | _ -> decisionProjection
+
+let apply events =
+    Seq.fold applyOne NoSubscription events
